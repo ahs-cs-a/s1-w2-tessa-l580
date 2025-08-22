54ac828deaf6169ffdcce0d7f59c017c3d9364b6
Tessa <tmlee0604@gmail.com>
2025-08-22 13:44:50 -0700

    homework 1

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,27 @@
 
+public class BodyOfWater{
+    private String name;
+    private int largestDiameter;
+    private double avgDepth;
+    private boolean isSaltWater;
+    
+    public BodyOfWater(String n, int l, double a, boolean s){
+        name = n;
+        largestDiameter = l;
+        avgDepth = a;
+        isSaltWater = s;
+
+    }
+    public BodyOfWater(String n, double a){
+        name = n;
+        avgDepth = a;
+    }
+    public void setName(String n) {
+        name = n;
+        
+    }
+    public void setLargestDiameter(int l)
+    largestDiameter = l;
+         
+    }
+
